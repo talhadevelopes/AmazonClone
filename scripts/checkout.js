dea935580907c601274e399c6fb7769b7c5a0837
Mtalha10k <trainedbot10k@gmail.com>
2024-12-15 15:33:23 +0530

    Load Products in beforeAll Hook

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,12 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+
+//this is annonymous func - a func without name
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
