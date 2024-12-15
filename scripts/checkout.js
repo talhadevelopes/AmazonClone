ec35e53b2d2ae05cb4826d31543e9c7f372d119d
Mtalha10k <trainedbot10k@gmail.com>
2024-12-15 16:45:58 +0530

    Implemented Error Handling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,55 +1,75 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-//array of promises - Promise.all()
-Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value 1');
-        });
-    }),
+async function loadPage() {
+    try {
+        // throw 'error1';
 
-    new Promise((resolve) => {
-        loadCart(() => {
-            resolve();
-        });
-    })
+        await loadProductsFetch();
 
-]).then((values) => {
-    console.log(values);
+        const value = await new Promise((resolve, reject) => {
+            // throw 'error2';
+            loadCart(() => {
+                // reject('error3');
+                resolve('value3');
+            });
+        });
+
+    } catch (error) {
+        console.log('Unexpected error. Please try again later.');
+    }
+
     renderOrderSummary();
     renderPaymentSummary();
-})
+}
+loadPage();
 
+/*
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
 
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
 
-//when we create a promise it runs the inner function immediaely
-//allows js to do multiple things at the same time
-// new Promise((resolve) => {
-//     loadProducts(() => {
-//         resolve('value 1');
-//     });
-// })
-//     .then((value) => {
-//         console.log(value);
-//         return new Promise((resolve) => {
-//             loadCart(() => {
-//                 resolve();
-//             });
-//         });
-//     })
-//     .then(() => {
-//         renderOrderSummary();
-//         renderPaymentSummary();
-//     });
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
 
+}).then((value) => {
+  console.log(value);
 
-//this is annonymous func - a func without name
-// loadProducts(() => {
-//     renderOrderSummary();
-//     renderPaymentSummary();
-// });
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
